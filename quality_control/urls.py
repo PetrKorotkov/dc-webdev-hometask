15f5e36ad09265d85304b0db5c03e8a8e19d3beb
Пётр Коротков <Petr2019Korotkov@yandex.ru>
2024-04-08 21:07:26 +0300

    commit before CRUD

--- quality_control/urls.py
+++ quality_control/urls.py
@@ -6,12 +6,20 @@ app_name = 'quality_control'
 urlpatterns = [
     #path('', views.index),
     path('', views.IndexView.as_view(), name='index'),
+
     #path('bugs/', views.bugs_list, name='bugs_list'),
     path('bugs/', views.BugListView.as_view(), name='bugs_list'),
+
     #path('requests/', views.features_list, name='features_list'),
     path('requests/', views.FeatureListView.as_view(), name='features_list'),
+
     #path('bugs/<int:bug_id>/', views.bug_detail, name='bug_detail'),
     path('bugs/<int:bug_id>/', views.BugDetailView.as_view(), name='bug_detail'),
+
     #path('requests/<int:feature_id>/', views.feature_detail, name='feature_detail')
-    path('requests/<int:feature_id>/', views.FeatureDetailView.as_view(), name='feature_detail')
+    path('requests/<int:feature_id>/', views.FeatureDetailView.as_view(), name='feature_detail'),
+
+    path('bug/new/', views.create_bugreport, name='create_bugreport'),
+
+    path('featurerequest/new/', views.create_featurerequest, name='create_featurerequest'),
 ]
--- quality_control/views.py
+++ quality_control/views.py
@@ -1,9 +1,9 @@
 from django.http import HttpResponse
 from django.urls import reverse
 from .models import BugReport, FeatureRequest
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, render, redirect
 from django.views.generic import DetailView, View, ListView
-from django.shortcuts import render
+from .forms import BugReportForm, FeatureRequestForm
 
 def index(request):
     return render(request, 'quality_control/index.html')
@@ -49,3 +49,23 @@ class FeatureDetailView(DetailView):
     pk_url_kwarg = 'feature_id'
     context_object_name = 'feature'
     template_name = 'quality_control/feature_detail.html'
+
+def create_bugreport(request):
+    if request.method == 'POST':
+        form = BugReportForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('quality_control:bugs_list')
+    else:
+        form = BugReportForm()
+    return render(request, 'quality_control/bug_report_form.html', {'form': form})
+
+def create_featurerequest(request):
+    if request.method == 'POST':
+        form = FeatureRequestForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('quality_control:features_list')
+    else:
+        form = FeatureRequestForm()
+    return render(request, 'quality_control/feature_request_form.html', {'form': form})
